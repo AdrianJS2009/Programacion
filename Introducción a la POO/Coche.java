6097ceda10780054f3de95bcd821ff485a0548a1
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-10-17 17:27:07 +0200

    Añadido ejemplo de clase que contiene dos objetos

---
+++ Introducción a la POO/Coche.java
@@ -0,0 +1,35 @@
+/*
+ * Creación de la clase Coche
+ * 
+ * @author Adrián Jiménez Santiago
+ */
+
+public class Coche {
+
+    //Atributos
+    String marca, modelo, color, matricula;
+    int numeroPuertas;
+
+    //Métodos
+    public void arrancar(){
+        System.out.println("Vroom~~ Vroom~~ ");
+    }
+
+    public void velocidadMaxima(){
+        System.out.println("Mi velocidad máxima es de 130 km/h");
+    }
+
+    public void sonarClaxon(){
+        System.out.println("Piiii~~ Piiiiii~~");
+    }
+
+    public static void main(String[] args){
+        //Creamos dos objetos de la clase Coche
+        Coche audi = new Coche();
+        Coche seat = new Coche();
+
+        audi.arrancar();
+        seat.velocidadMaxima();
+    }
+
+}
