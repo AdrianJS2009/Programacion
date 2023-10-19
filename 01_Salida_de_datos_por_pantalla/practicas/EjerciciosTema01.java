f7cd57b095d2740199a2b762718aecfb7c158977
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-10-19 17:21:55 +0200

    Implementado ejercicio 6

--- 01_Salida_de_datos_por_pantalla/practicas/EjerciciosTema01.java
+++ 01_Salida_de_datos_por_pantalla/practicas/EjerciciosTema01.java
@@ -48,11 +48,11 @@ public class EjerciciosTema01 {
                     break;
                 case "4": programa.Ejercicio4();
                     break;    
-                /* case "5": programa.Ejercicio5();
+                case "5": programa.Ejercicio5();
                     break;
                 case "6": programa.Ejercicio6();
                     break;
-                case "7": programa.Ejercicio7();
+                /* case "7": programa.Ejercicio7();
                     break;
                 case "8": programa.Ejercicio8();
                     break;
@@ -136,6 +136,87 @@ public class EjerciciosTema01 {
       System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "Sistemas Informáticos");
       System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "Sistemas Informáticos");
       System.out.println("--------------------------------------------");
+    }
+
+    public void Ejercicio5() {
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("--------------------------------------------");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Día", "Horas", "Asignaturas");
+      System.out.println("--------------------------------------------");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Lunes", "15:15-16:15", "\033[33m FOL");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "16:15-17:15", "\033[33m FOL");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "17:15-18:15", "\033[33m FOL");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "18:30-19:30", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("#######################################################");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Martes", "15:15-16:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "16:15-17:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "17:15-18:15", "\033[35m Bases de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "18:30-19:30", "\033[35m Bases de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "\033[32m Lenguaje de marcas");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "\033[32m Lenguaje de marcas");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("#######################################################");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Miércoles", "15:15-16:15", "\033[36m Entornos de Desarrollo");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "16:15-17:15", "\033[36m Entornos de Desarrollo");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "17:15-18:15", "\033[35m Bases de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "18:30-19:30", "\033[35m Bases de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("#######################################################");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Jueves", "15:15-16:15", "\033[35m Base de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "16:15-17:15", "\033[35m Base de Datos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "17:15-18:15", "\033[32m Lenguaje de marcas");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "18:30-19:30", "\033[32m Lenguaje de marcas");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("#######################################################");
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "Viernes", "15:15-16:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "16:15-17:15", "\033[31m Programación");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "17:15-18:15", "\033[36m Entornos de Desarollo");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "18:30-19:30", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "19:30-20:30", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.printf("| %-9s | %-5s | %-10s |\n", "", "20:30-21:15", "\033[34m Sistemas Informáticos");
+      System.out.println("\033[0m"); // Reseteo de color
+      System.out.println("--------------------------------------------");
+
+    }
+
+    public void Ejercicio6() {
+      System.out.println("    *    ");
+      System.out.println("   ***   ");
+      System.out.println("  *****  ");
+      System.out.println(" ******* ");
+      System.out.println("*********");
     }
   }
 
