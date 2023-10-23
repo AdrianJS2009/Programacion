a98f280c2e50b4b25f50292954281b7b52e5464a
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-10-23 19:50:02 +0200

    Implementación del ejercicio 5

--- 02_Variables/practicas/Ejercicio5.java
+++ 02_Variables/practicas/Ejercicio5.java
@@ -1,11 +1,17 @@
 /**
- * Explicación del código
+ * Lo mismo que el ejercicio anterior, pero convirtiendo euros a pesetas.
  * 
  * @author Adrián Jiménez Santiago
  */
 
 public class Ejercicio5 {
   public static void main(String[] args) {
-    
+    int pesetas = 54978;
+    double euros = pesetas / 166.386;
+
+      System.out.println(pesetas + " de pesetas convertidas a euros son: " + euros + " euros.");
+      System.out.printf("%d pesetas son %.2f euros.\n", pesetas, euros);
+
   }
+  
 }
