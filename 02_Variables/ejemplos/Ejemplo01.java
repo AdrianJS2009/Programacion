bd2da2c79f83ea4fa97c35694b4a0ba4700ffe3e
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-10-24 16:23:28 +0200

    Añadido el fichero de ejemplos del tema 2

---
+++ 02_Variables/ejemplos/Ejemplo01.java
@@ -0,0 +1,46 @@
+/**
+ * Explicación del código
+ * 
+ * @author Adrián Jiménez Santiago
+ */
+
+public class Ejemplo01 {
+  public static void main(String[] args) {
+
+    // Tipo byte
+    byte miNumByte = 100;
+    System.out.println("Mi número de tipo byte es: " + miNumByte);
+
+    // Tipo short
+    short miNumShort = 5200;
+    System.out.println("Mi número de tipo short es: " + miNumShort);
+
+    // Tipo long
+    long miNumLong = 150000000L;
+    System.out.println("Mi número de tipo long es: " + miNumLong);
+
+    // Tipo Float
+    float miNumFloat = 5.6f;
+    System.out.println("Mi número de tipo float es: " + miNumFloat);
+
+    // Tipo boolean
+    boolean miDatoBoolean = true;
+    System.out.println("¿Es programar en Java divertido? " + miDatoBoolean);
+
+    int numero = 5;
+
+    System.out.println("El valor de la variable es: " + numero);
+
+    double x;
+    double y;
+
+    x = 7;
+    y = 25.75;
+
+    System.out.println("El valor de x es: " + x);
+    System.out.println("El valor de y es: " + y);
+
+    int num1 = 9;
+    int num2 = 2;
+  }
+}
