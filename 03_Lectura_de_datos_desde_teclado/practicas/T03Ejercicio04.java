fd376fa7dbe762dd69869a742d0c35f86b152f24
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-10-27 17:33:34 +0200

    Implementacion del ejercicio4

---
+++ 03_Lectura_de_datos_desde_teclado/practicas/T03Ejercicio04.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Explicación del código
+ * 
+ * @author Adrián Jiménez Santiago
+ */
+
+public class T03Ejercicio04 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Introduce el primer número con el que operar");
+    int num1 = sc.nextInt();
+
+    System.out.println("Introduce el segundo número con el que operar");
+    int num2 = sc.nextInt();
+
+    int suma = num1 + num2;
+    int resta = num1 - num2;
+    int multiplicacion = num1 * num2;
+    float division = num1 / num2;
+
+    System.out.println("La suma de ambos números es: " + suma);
+    System.out.println("La resta de ambos números es: " + resta);
+    System.out.println("La multiplicación de ambos números es: " + multiplicacion);
+    System.out.println("La división de ambos números es: " + division);
+
+  }
+}
