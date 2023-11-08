deb62ae2b5d3bf60c94929764cd527d1e4c455f6
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2023-11-08 18:19:39 +0100

    Corregido ejercicio 6

--- 04_Sentencia_Condicional_(If y Switch)/practicas/T04Ejercicio06.java
+++ 04_Sentencia_Condicional_(If y Switch)/practicas/T04Ejercicio06.java
@@ -15,7 +15,14 @@ public class T04Ejercicio06 {
     System.out.println("Vamos a calcular el tiempo que tarda en caer un objeto, indicame una altura:");
     float altura = sc.nextFloat();
 
+    if (altura <= 0) {
+      System.out.println("La altura no puede ser igual o menor que 0");
+    }
+
     double resultado = Math.sqrt(2 * altura / g);
+    if (resultado < 0) {
+      System.out.println("Resultado inváido");
+    }
 
     System.out.println("El objeto tarda en caer: " + resultado + " segundos");
 
